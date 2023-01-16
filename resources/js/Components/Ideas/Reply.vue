bbad81da7e1f9483759e58695f82fcc0a3370080
Tieu Cam Thanh <tieucamthanh@vigilantsolutions.com>
2023-01-16 20:55:44 +0700

    remove un use code

--- resources/js/Components/Ideas/Reply.vue
+++ resources/js/Components/Ideas/Reply.vue
@@ -1,18 +1,7 @@
 <template>
     <Popover class="relative">
         <HighOrderFloat>
-        <!-- portal="#popover"
-        enter="transition duration-100 ease-out"
-        enter-from="transform scale-95 opacity-0"
-        enter-to="transform scale-100 opacity-100"
-        leave="transition duration-75 ease-out"
-        leave-from="transform scale-100 opacity-100"
-        leave-to="transform scale-95 opacity-0"
-        tailwindcss-origin-class -->
-            <PopoverButton
-                esc
-                as="div"
-            >
+            <PopoverButton esc as="div">
                 <AppPrimaryButton width="w-32" size="text-sm">Reply</AppPrimaryButton>
             </PopoverButton>
 
