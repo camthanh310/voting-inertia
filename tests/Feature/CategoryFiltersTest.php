eae777d35f81c82078346f63897c3344cd42fe11
Tieu Cam Thanh <tieucamthanh@vigilantsolutions.com>
2023-01-27 20:16:02 +0700

    Category Filter Test

--- tests/Feature/CategoryFiltersTest.php
+++ tests/Feature/CategoryFiltersTest.php
@@ -1,9 +1,9 @@
 <?php
 
-use App\Models\Category;
 use App\Models\Idea;
-use App\Models\Status;
 use App\Models\User;
+use App\Models\Status;
+use App\Models\Category;
 use Inertia\Testing\AssertableInertia;
 
 use function Pest\Laravel\get;
@@ -12,6 +12,7 @@ it('filtering works when query string in place', function () {
     $user = User::factory()->create();
 
     $categoryOne = Category::factory()->create(['name' => 'Category 1']);
+    $categoryTwo = Category::factory()->create(['name' => 'Category 2']);
 
     $statusOpen = Status::factory()->create(['name' => 'Open']);
     $statusConsidering = Status::factory()->create(['name' => 'Considering', 'classes' => 'bg-purple text-white']);
@@ -36,7 +37,7 @@ it('filtering works when query string in place', function () {
     Idea::factory()
         ->create([
             'user_id' => $user->id,
-            'category_id' => $categoryOne->id,
+            'category_id' => $categoryTwo->id,
             'status_id' => $statusConsidering->id
         ]);
 
@@ -50,7 +51,7 @@ it('filtering works when query string in place', function () {
     Idea::factory()
         ->create([
             'user_id' => $user->id,
-            'category_id' => $categoryOne->id,
+            'category_id' => $categoryTwo->id,
             'status_id' => $statusImplemented->id
         ]);
 
@@ -64,14 +65,16 @@ it('filtering works when query string in place', function () {
     get(
         route('idea.index', [
             'filter' => [
-                'status_id' => Status::IMPLEMENTED
+                'status_id' => Status::IMPLEMENTED,
+                'category_id' => $categoryTwo->id
             ]
-        ])
+        ]),
     )
     ->assertInertia(
         fn (AssertableInertia $assert) => $assert
             ->component('Idea/Index')
-            ->has('ideas.data', 3)
+            ->has('ideas.data', 1)
             ->where('ideas.data.0.status.id', Status::IMPLEMENTED)
+            ->where('ideas.data.0.category.name', $categoryTwo->name)
     );
 });
